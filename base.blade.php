27beaa3af0fd8760978f661ab8d7ec1bb9c0f838
Leo_3 <84869118+a44097@users.noreply.github.com>
2022-02-04 18:17:56 +0000

    todos os layout e as ligações
    falta somente dar id corretos para facilitar na ligação com o banco de dados

---
+++ base.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>index</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light pt-0" style="background-color: #74C69D;" >
+        
+        <div class="row text-success fs-5 fw-bold text-start p-1 "><a class="nav-link" href="/"> G FARM</a></div>
+                    
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav text-start">
+                    <li class="nav-item fw-bold">
+                    <a class="nav-link ps-5" href="campo">Campo</a>
+                    </li>
+                    <li class="nav-item fw-bold">
+                    <a class="nav-link" href="trabalhador">Trabalhadores</a>
+                    </li>
+                    <li class="nav-item fw-bold">
+                    <a class="nav-link " href="producao">Produção</a>
+                    </li>
+                    <li class="nav-item fw-bold">
+                    <a class="nav-link" href="tarefas">Tarefas</a>
+                    </li>                                     
+                 </ul>
+    
+                 <div class="row col-8"></div>
+                 <button class="btn btn-light me-3 ">Login</button>            
+           
+            </div>
+        </div>
+    </nav>
+
+    @yield('body')
+</body>
+
+    <script src="/js/bootstrap.min.js"></script>
+
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top" >
+        <div class="col-md-4 d-flex align-items-center">
+        <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+            <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+        <span class="text-muted">© 2021 Company, Inc</span>
+        </div>
+    <div class="col-md-8 d-flex align-items-center"> Desenvolvedores: Carlos Wilson e Leandro Santos</div>   
+  </footer>
+
+</html>
---
+++ dashboard.blade.php
@@ -0,0 +1,232 @@
+@extends('base')
+@section('body')
+<!-- Background image -->
+<div class="row pt-5 ps-8" style="background-image:{{asset('img/fundo_forms.jpg')}};">
+<!-- Background image -->
+
+<!-- Content Row -->
+<div class="row" style="text-align: center;padding-left: 2% ;padding-right: 2%;">
+
+  <!-- Earnings (Monthly) Card Example -->
+  <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-left-primary shadow h-100 py-2">
+          <div class="card-body">
+              <div class="row no-gutters align-items-center">
+                  <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                          produção (Mensais)</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">Toneladas(campo calculado)</div>
+                  </div>
+                  <div class="col-auto">
+                      <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+
+  <!-- Earnings (Monthly) Card Example -->
+  <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-left-success shadow h-100 py-2">
+          <div class="card-body">
+              <div class="row no-gutters align-items-center">
+                  <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                        Produção (Annual)</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">Toneladas(campo calculado)</div>
+                  </div>
+                  <div class="col-auto">
+                      <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+
+  <!-- Earnings (Monthly) Card Example -->
+  <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-left-info shadow h-100 py-2">
+          <div class="card-body">
+              <div class="row no-gutters align-items-center">
+                  <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tarefas Feitas
+                      </div>
+                      <div class="row no-gutters align-items-center">
+                          <div class="col-auto">
+                              <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                          </div>
+                          <div class="col">
+                              <div class="progress progress-sm mr-2">
+                                  <div class="progress-bar bg-info" role="progressbar"
+                                      style="width: 50%" aria-valuenow="50" aria-valuemin="0"
+                                      aria-valuemax="100"></div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-auto">
+                      <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+
+  <!-- Pending Requests Card Example -->
+  <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-left-warning shadow h-100 py-2">
+          <div class="card-body">
+              <div class="row no-gutters align-items-center">
+                  <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Tarefas Planejadas</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                  </div>
+                  <div class="col-auto">
+                      <i class="fas fa-comments fa-2x text-gray-300"></i>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+
+ <!--tarefas para hoje-->
+
+ <div class="conteiner ">         
+    <div class="row ps-5 px-5">   
+       <div class="col-6">
+       <div class="card shadow mb-4 -d-flex flex-column align-items-center flex-shrink-0 py-2 border border-dark" style="background-color: #74C69D; width: 100%;">              
+                   <a href="#" ></a>
+                       <span class="fs-3 fw-bold text-center py-3">Lista de tarefas de hoje</span>
+                   </a>
+                   <div class="list-group list-group-flush border-bottom scrollarea">
+                   <a href="#" class="list-group-item list-group-item-action py-5 lh-tight" aria-current="true">
+                       <div class="d-flex w-100 align-items-center justify-content-between">
+                       <strong class="mb-1">tarefa 1</strong>
+                       <small>$date</small>
+                       </div>
+                       <div class="col-10 mb-1 small">especificacoes</div>
+                   </a>
+                   <a href="#" class="list-group-item list-group-item-action py-5 lh-tight">
+                       <div class="d-flex w-100 align-items-center justify-content-between">
+                       <strong class="mb-1">tarefa 2</strong>
+                       <small class="text-muted">$date</small>
+                       </div>
+                       <div class="col-10 mb-1 small">...</div>
+                   </a>
+                   <a href="#" class="list-group-item list-group-item-action py-5 lh-tight">
+                       <div class="d-flex w-100 align-items-center justify-content-between">
+                       <strong class="mb-1">tarefa 3</strong>
+                       <small class="text-muted">$date</small>
+                       </div>
+                       <div class="col-10 mb-1 small">...</div>
+                   </a>
+               
+                   <a href="#" class="list-group-item list-group-item-action py-5 lh-tight" aria-current="true">
+                       <div class="d-flex w-100 align-items-center justify-content-between">
+                       <strong class="mb-1">tarefa 4</strong>
+                       <small class="text-muted">$date</small>
+                       </div>
+                       <div class="col-10 mb-1 small">...</div>
+                   </a>
+                   
+                   <a href="#" class="list-group-item list-group-item-action py-5 lh-tight" aria-current="true">
+                       <div class="d-flex w-100 align-items-center justify-content-between ">
+                       <strong class="mb-1">tarefa n</strong>
+                       <small class="text-muted">$date</small>
+                       </div>
+                       <div class="col-10 mb-1 small">ficará a quantidade de tarefas programdas em tarefas</div>
+                   </a>   
+                                          
+                
+                       </div>
+                           </div>
+                               </div>
+
+     <!-- Project Card Example -->
+        <div class="conteiner  mb-3 col-6 p-5 ">    
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Projects</h6>
+                </div>
+                <div class="card-body">
+                    <h4 class="small font-weight-bold">
+                        projeto <span class="float-right">20%</span>
+                    </h4>
+                <div class="progress mb-4">
+                    <div class="progress-bar bg-danger" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
+                    </div>
+                </div>
+                <h4 class="small font-weight-bold">projeto 2 <span class="float-right">40%</span></h4>
+                <div class="progress mb-4">
+                                    <div class="progress-bar bg-warning" role="progressbar" style="width: 40%"
+                                        aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <h4 class="small font-weight-bold">projeto 3<span
+                                        class="float-right">60%</span></h4>
+                                <div class="progress mb-4">
+                                    <div class="progress-bar" role="progressbar" style="width: 60%"
+                                        aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <h4 class="small font-weight-bold">projeto 4 <span
+                                        class="float-right">80%</span></h4>
+                                <div class="progress mb-4">
+                                    <div class="progress-bar bg-info" role="progressbar" style="width: 80%"
+                                        aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <h4 class="small font-weight-bold">projeto n<span
+                                        class="float-right">Complete!</span></h4>
+                                <div class="progress">
+                                    <div class="progress-bar bg-success" role="progressbar" style="width: 100%"
+                                        aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div> 
+                        </div> 
+
+                        <div class="card shadow mb-4 ">
+                            <table class="table">
+                                <div class="fs-4 fw-bold text-center ">Lista dos melhores trabalhadores</div>
+                                <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">Apelido</th>
+                                    <th scope="col">User</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">3</th>
+                                        <td colspan="2">Larry the Bird</td>
+                                        <td>@twitter</td>
+                                    </tr>
+                                </tbody>
+                                <div class="row mb-3"></div>
+                            </table>               
+                            </div>
+                            <div class="card shadow mb-5">
+                            <div class="row justify-content-center align-items-center">
+                                
+                                <div class="row text-center pb-3">
+                                    <form>
+                                        <h5>Subscreva para o nosso newsletter</h5>
+                                        <p>Seja nosso cliente e venha optmizar com a gente</p>
+                                        <div class="d-flex w-100 gap-2">
+                                            <label for="newsletter1" class="visually-hidden">Email address</label>
+                                            <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+                                            <button class="btn btn-success" type="button">Subscreva</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>   
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+           
+                
+
+@endsection
